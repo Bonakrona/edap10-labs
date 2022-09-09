152952ce294171e9adf037fc4e20a78719499991
User <User@MSI>
2022-09-09 08:13:00 +0100

    Broderlig början på train

--- widgets/src/train/simulation/TrainSimulation.java
+++ widgets/src/train/simulation/TrainSimulation.java
@@ -1,5 +1,8 @@
 package train.simulation;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 import train.model.Route;
 import train.model.Segment;
 import train.view.TrainView;
@@ -7,14 +10,37 @@ import train.view.TrainView;
 public class TrainSimulation {
 
     public static void main(String[] args) {
+    	
 
+    	
+    	
         TrainView view = new TrainView();
         
-        Route route = view.loadRoute();
+        SegmentAssistant segmentAssist = new SegmentAssistant();
         
+    	Route route = view.loadRoute();
+    	ArrayList<Segment> trainList = new ArrayList<Segment>();
         Segment first = route.next();
-        
-        first.enter();
+        for (int i = 0; i < 3; i++) {
+        	trainList.add(first);
+        	first.enter();
+        	first = route.next();
+	        
+	    }
     }
-
 }
+
+/*
+you need write code to
+• create a queue,
+• load a route, view.loadRoute()
+• read three segments from the route, route.next()
+• store them in your queue, and
+• mark them as busy. segment.enter()
+To get the train moving, your code will need to repeatedly ------------------------ här är du
+• read the next segment from the route, route.next()
+• mark it as busy, head.enter()
+• add it to the head of your queue,
+• remove the tail Segment from your queue, and
+• mark the removed segment as free. tail.exit()
+*/
